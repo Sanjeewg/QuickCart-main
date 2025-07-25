50705ba4c32463e33803e1ee6e218a9f3b10aaf2
Sanjeewg <sanjeewg890@gmail.com>
2025-07-25 13:06:43 +0530

    first Commit

---
+++ models/User.js
@@ -0,0 +1,16 @@
+import types from "@eslint/eslintrc/lib/shared/types";
+import mongoose from "mongoose";
+import { unique } from "next/dist/build/utils";
+
+const userSchema = new mongoose.Schema({
+    _id:{ type:String,required:true},
+    name:{ type:String,required:true},
+    email:{ type:String,required:true,unique:true},
+    imageUrl:{ type:String,required:true},
+    cartItems:{types:Object,default:{}}
+},{minimize: false})
+
+const User =mongoose.models.User || mongoose.model('user',userSchema)
+
+
+export default User
